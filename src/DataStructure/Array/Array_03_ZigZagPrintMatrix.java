eda3200f9a787da44bf3638672ec9f8ffc142e8d
Huangxiaoyuyuyu <253757635@qq.com>
2019-03-04 13:35:07 +0800

    Array_03_ZigZagPrintMatrix~

--- src/DataStructure/Array/Array_03_ZigZagPrintMatrix.java
+++ src/DataStructure/Array/Array_03_ZigZagPrintMatrix.java
@@ -8,6 +8,17 @@ package DataStructure.Array;
  */
 public class Array_03_ZigZagPrintMatrix {
 
+    /**
+     * “之”字形打印矩阵
+     *    给定一个矩阵matrix，按照“之”字形的方式打印这
+     * 个矩阵，例如： 1   2   3   4
+     *               5   6   7   8
+     *               9   10 11  12
+     * “之”字形打印的结果为：1，2，5，9，6，3，4，7，10，11，8，12
+     * 要求：
+     *  额外空间复杂度为O(1)。
+     * @param matrix
+     */
     public static void printMatrixZigZag(int[][] matrix) {
         int tR = 0;
         int tC = 0;

99b963859f5c87b617328b56c524c826ac0575b0
huangxiaoyuyuyu <253757635@qq.com>
2019-03-10 22:33:05 +0800

    Tree_01_PreInPostOrderRecur

---
+++ src/DataStructure/Tree/Tree_01_PreInPostOrderRecur.java
@@ -0,0 +1,74 @@
+package DataStructure.Tree;
+
+/**
+ * @Author OliverYu
+ * @Date 2019/3/10 21:26
+ * @Email 253757635@qq.com
+ * @Description TODO
+ */
+public class Tree_01_PreInPostOrderRecur {
+
+    /**
+     * 递归方式实现二叉树的先序、中序、后序遍历
+     */
+    public static class Node {
+        public int data;
+        public Node left;
+        public Node right;
+
+        public Node(int data) {
+            this.data = data;
+        }
+    }
+
+    public static void preOrderRecur(Node head) {
+        if (head == null) {
+            return;
+        }
+        System.out.print(head.data + " ");
+        preOrderRecur(head.left);
+        preOrderRecur(head.right);
+    }
+
+    public static void inOrderRecur(Node head) {
+        if (head == null) {
+            return;
+        }
+        inOrderRecur(head.left);
+        System.out.print(head.data + " ");
+        inOrderRecur(head.right);
+    }
+
+    public static void postOrderRecur(Node head) {
+        if (head == null) {
+            return;
+        }
+        postOrderRecur(head.left);
+        postOrderRecur(head.right);
+        System.out.print(head.data + " ");
+    }
+
+    public static void main(String[] args) {
+        Node head = new Node(5);
+        head.left = new Node(3);
+        head.right = new Node(8);
+        head.left.left = new Node(2);
+        head.left.right = new Node(4);
+        head.left.left.left = new Node(1);
+        head.right.left = new Node(7);
+        head.right.left.left = new Node(6);
+        head.right.right = new Node(10);
+        head.right.right.left = new Node(9);
+        head.right.right.right = new Node(11);
+
+        System.out.println("==============Recursive==============");
+        System.out.print("pre-order: ");
+        preOrderRecur(head);
+        System.out.println();
+        System.out.print("in-order: ");
+        inOrderRecur(head);
+        System.out.println();
+        System.out.print("pos-order: ");
+        postOrderRecur(head);
+    }
+}
